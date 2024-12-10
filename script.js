59834ce411325dda8074f1ffaf1d106d69f0a112
adityamiddha <132482055+adityamiddha@users.noreply.github.com>
2024-12-10 12:14:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Function to generate a random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Function to change the background color of the body
+function changeColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
